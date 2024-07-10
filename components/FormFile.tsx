eacb53645335287f5261cd10b5a76c3fcdd7823a
RafaM-dev <rafaelmonroy002@gmail.com>
2024-07-10 11:33:18 -0500

    first commit

---
+++ components/FormFile.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FormFile = () => {
+    return (
+        <View className='space-y-2'>
+            <Text>FormFile</Text>
+        </View>
+    )
+}
+
+export default FormFile
